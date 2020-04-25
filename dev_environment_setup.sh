9d51ca45ebac8399eb53e34c23b70cce88d4fca4
Ubuntu <ubuntu@ip-172-31-93-221.ec2.internal>
2020-04-25 06:39:21 +0000

    Committing to try on a fresh machine

--- dev_environment_setup.sh
+++ dev_environment_setup.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
+# Get chromedriver
+curl -SL https://chromedriver.storage.googleapis.com/2.32/chromedriver_linux64.zip > chromedriver.zip
+unzip -o chromedriver.zip -d bin/
+
+# Get Headless-chrome
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-29/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip -o headless-chromium.zip -d bin/
+
 sudo apt install python3-pip zip unzip 
 
-# download Selenium 2.37
-$ pip3.6 install -t seleniumLayer/selenium/python/lib/python3.6/site-packages requirements
+mkdir --parents lib bin
 
-# download chrome driver
-cd seleniumLayer
-mkdir chromedriver
-cd chromedriver
-curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
-unzip -o chromedriver.zip
-rm chromedriver.zip
+pip3 install -r requirements.txt -t lib
 
-# download chrome binary
-curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
-unzip -o headless-chromium.zip
-rm headless-chromium.zip
-
-sudo apt install npm
-npm config set prefix /usr/local
-sudo npm i -g serverless
-
-
-export PATH=${PATH}:~/Selenium-UI-testing-with-AWS-Lambda-Layers/chromedrive
