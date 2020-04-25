5626575e36eb55b17df19e4b10d6d37b44367542
Ubuntu <ubuntu@ip-172-31-93-221.ec2.internal>
2020-04-25 03:33:57 +0000

    Starting using serverless to deal with lambda

--- dev_environment_setup.sh
+++ dev_environment_setup.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-sudo apt install python3-pip
+sudo apt install python3-pip zip unzip 
 
-pip3 install -r requirements.txt -t lib
-PYTHONPATH=~/nz-river-level-alerts/lib
+# download Selenium 2.37
+$ pip3.6 install -t seleniumLayer/selenium/python/lib/python3.6/site-packages requirements
+
+# download chrome driver
+cd seleniumLayer
+mkdir chromedriver
+cd chromedriver
+curl -SL https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip > chromedriver.zip
+unzip -o chromedriver.zip
+rm chromedriver.zip
+
+# download chrome binary
+curl -SL https://github.com/adieuadieu/serverless-chrome/releases/download/v1.0.0-41/stable-headless-chromium-amazonlinux-2017-03.zip > headless-chromium.zip
+unzip -o headless-chromium.zip
+rm headless-chromium.zip
+
+sudo apt install npm
+npm config set prefix /usr/local
+sudo npm i -g serverless
+
+
+export PATH=${PATH}:~/Selenium-UI-testing-with-AWS-Lambda-Layers/chromedrive
